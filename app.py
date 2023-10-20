0d523fe80c57fe2c8baea23e21af4272563402c4
ANAS NOURI <anasnouriia@gmail.com>
2023-10-20 18:39:16 +0100

    modeldeploy

---
+++ app.py
@@ -0,0 +1,56 @@
+import pickle
+from xgboost import XGBClassifier
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_folder='static')
+
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Define your list of input feature names
+    input_names = ['retained earnings / total assets', 'sales / total assets',
+                   '(total liabilities * 365) / (gross profit + depreciation)',
+                   'sales (n) / sales (n-1)',
+                   'profit on operating activities / total assets',
+                   'profit on operating activities / financial expenses',
+                   'total sales / total assets',
+                   '(current assets - inventories) / long-term liabilities',
+                   'profit on operating activities / sales',
+                   'rotation receivables + inventory turnover in days',
+                   '(inventory * 365) / cost of products sold', 'working capital',
+                   'total costs / total sales', 'long-term liabilities / equity',
+                   'sales / inventory', 'sales / fixed assets']
+    
+    if request.method == 'POST':
+        inputs = np.array([])
+        for name in input_names:
+            input_value = request.form.get(name)
+            if input_value is not None:
+                input_data = float(input_value)
+                inputs = np.append(inputs, input_data)
+        data = {name: [value] for name, value in zip(input_names, inputs)}
+        df = pd.DataFrame(data)
+        df.index = ['Row1']
+
+        if len(inputs) == len(input_names):
+            # Make the prediction if all input values were successfully collected
+            prediction = model.predict(df)
+            return f"Prediction: {prediction}"
+        else:
+            print("len inputs: ")
+            print(len(inputs))
+            print("\n")
+            print("len input_names: ")
+            print(len(input_names))
+            return f"Prediction: none"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,43 @@
+# Import necessary libraries
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+def load_and_preprocess_data(file_path):
+    # Load your dataset from the specified file path
+    data = pd.read_csv(file_path)
+    
+    # Separate the features (X) and target variable (y)
+    X = data.drop('bankruptcy status', axis=1)
+    y = data['bankruptcy status']
+    
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+def train_xgboost_model(X_train, y_train):
+    # Initialize and train your XGBoostClassifier model
+    xgb_classifier = XGBClassifier()
+    xgb_classifier.fit(X_train, y_train)
+    
+    return xgb_classifier
+
+if __name__ == "__main__":
+    # Define the path to your dataset
+    dataset_path = 'data_perentille.csv'  # Replace with the actual path to your dataset
+    
+    # Load and preprocess the data
+    X_train, X_test, y_train, y_test = load_and_preprocess_data(dataset_path)
+    
+    # Train the XGBoost model
+    xgb_model = train_xgboost_model(X_train, y_train)
+    
+    # Optionally, you can save the trained model to a file
+    # xgb_model.save_model('xgb_model.model')
+    predictions = xgb_model.predict(X_test)
+    xgb_model.score(X_test,y_test)
+    #save the model
+    file = open("model.pkl", 'wb')
+    pickle.dump(xgb_model, file)
