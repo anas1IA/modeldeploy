daefba3b3f3842dac32d2e2f3eff38c51a0bc87a
ANAS NOURI <anasnouriia@gmail.com>
2023-10-21 16:06:21 +0100

    chnging

--- model.py
+++ model.py
@@ -2,13 +2,13 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier  # Import XGBoost
+from sklearn.ensemble import RandomForestClassifier  # Import RandomForestClassifier
 
 def load_and_preprocess_data(file_path):
     # Load your dataset from the specified file path
     data = pd.read_csv(file_path)
     
-    # Separate the features (X) and target variable (y)
+    # Separate the features (X) and the target variable (y)
     X = data.drop('bankruptcy status', axis=1)
     y = data['bankruptcy status']
     
@@ -17,12 +17,12 @@ def load_and_preprocess_data(file_path):
     
     return X_train, X_test, y_train, y_test
 
-def train_xgboost_model(X_train, y_train):
-    # Initialize and train your XGBoostClassifier model
-    xgb_classifier = XGBClassifier()  # Use XGBClassifier here
-    xgb_classifier.fit(X_train, y_train)
+def train_random_forest_model(X_train, y_train):
+    # Initialize and train your RandomForestClassifier model
+    rf_classifier = RandomForestClassifier()  # Use RandomForestClassifier here
+    rf_classifier.fit(X_train, y_train)
     
-    return xgb_classifier
+    return rf_classifier
 
 if __name__ == "__main__":
     # Define the path to your dataset
@@ -31,15 +31,12 @@ if __name__ == "__main__":
     # Load and preprocess the data
     X_train, X_test, y_train, y_test = load_and_preprocess_data(dataset_path)
     
-    # Train the XGBoost model
-    xgb_model = train_xgboost_model(X_train, y_train)
+    # Train the Random Forest model
+    rf_model = train_random_forest_model(X_train, y_train)
     
-    # Optionally, you can save the trained model to a file using XGBoost's built-in save_model method
-    xgb_model.save_model('xgb_model.model')
+    # Optionally, you can save the trained model to a file using the pickle module
+    with open("model.pkl", 'wb') as file:
+        pickle.dump(rf_model, file)
     
-    predictions = xgb_model.predict(X_test)
-    accuracy = xgb_model.score(X_test, y_test)
-    
-    # Save the model using the pickle module
-    with open("modele.pkl", 'wb') as file:
-        pickle.dump(xgb_model, file)
+    predictions = rf_model.predict(X_test)
+    accuracy = rf_model.score(X_test, y_test)
