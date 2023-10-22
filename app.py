b9999420274b63e4da18dade233d8d05b8e2f430
ANAS NOURI <anasnouriia@gmail.com>
2023-10-22 17:50:40 +0100

    LAST ONeEww445543ae3l4233

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import joblib
-import pickle
 import pandas as pd
 import numpy as np
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-with open('xgboost_model.pkl', 'rb') as model_file:
+with open('random_forest_model.pkl', 'rb') as model_file:
     model = joblib.load(model_file)
 
 # Define phrases for predictions
